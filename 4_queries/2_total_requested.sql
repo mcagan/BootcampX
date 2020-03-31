7c1c9849239a8a1132fa45939cace8e1dc2ed8ad
mcagan <meliscagan@gmail.com>
2020-03-31 14:24:43 +0000

    implement query for total assistances per student

---
+++ 4_queries/2_total_requested.sql
@@ -0,0 +1,4 @@
+SELECT count(assistance_requests.*) as total_assistances, students.name
+FROM students JOIN assistance_requests ON students.id = assistance_requests.student_id
+WHERE students.name = 'Elliot Dickinson'
+GROUP BY students.name;
