f32ecf50b8b35d2574890880f06cf2aab260bfda
mcagan <meliscagan@gmail.com>
2020-03-31 14:22:49 +0000

    implement query for total assistances per teacher

---
+++ 4_queries/1_total_responded.sql
@@ -0,0 +1,4 @@
+SELECT count(assistance_requests.*) as total_assistances, teachers.name
+FROM teachers JOIN assistance_requests ON teachers.id = assistance_requests.teacher_id
+WHERE teachers.name = 'Waylon Boehm'
+GROUP BY teachers.name;
